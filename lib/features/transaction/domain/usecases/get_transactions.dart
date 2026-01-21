59292d9309d5f91de0fc50d9c814b4a9ac65b477
Renan Martins <renanmdreis@alu.ufc.br>
2026-01-21 18:42:11 -0300

    feat (domain): add get transactions usecase on the domain layer and setUp get transactions test

---
+++ lib/features/transaction/domain/usecases/get_transactions.dart
@@ -0,0 +1,7 @@
+import 'package:smart_wallet/features/transaction/domain/repositories/transaction_repository.dart';
+
+class GetTransactions {
+  final TransactionRepository repository;
+
+  GetTransactions(this.repository);
+}
--- test/features/transaction/domain/usecases/get_transactions_test.dart
+++ test/features/transaction/domain/usecases/get_transactions_test.dart
@@ -1,5 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:smart_wallet/features/transaction/domain/repositories/transaction_repository.dart';
+import 'package:smart_wallet/features/transaction/domain/usecases/get_transactions.dart';
 
+class MockTransactionRepository extends Mock implements TransactionRepository {}
 
 void main() {
-  
+  GetTransactions usecase;
+  MockTransactionRepository mockTransactionRepository;
+  setUp(() {
+    mockTransactionRepository = MockTransactionRepository();
+    usecase = GetTransactions(mockTransactionRepository);
+  });
 }
