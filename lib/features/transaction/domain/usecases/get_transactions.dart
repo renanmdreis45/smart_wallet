395b7b1e5f0ef402e25805caa03d4e3014e687cb
Renan Martins <renanmdreis@alu.ufc.br>
2026-01-24 19:10:22 -0300

    feat (test): finish get transactions test

--- lib/features/transaction/domain/usecases/get_transactions.dart
+++ lib/features/transaction/domain/usecases/get_transactions.dart
@@ -1,7 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:smart_wallet/core/error/failure.dart';
+import 'package:smart_wallet/features/transaction/domain/entities/transaction.dart';
 import 'package:smart_wallet/features/transaction/domain/repositories/transaction_repository.dart';
 
 class GetTransactions {
   final TransactionRepository repository;
 
   GetTransactions(this.repository);
+
+  Future<Either<Failure, List<Transaction>>> call() async {
+    return await repository.getTransactions();
+  }
 }
--- test/features/transaction/domain/usecases/get_transactions_test.dart
+++ test/features/transaction/domain/usecases/get_transactions_test.dart
@@ -1,15 +1,33 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:smart_wallet/features/transaction/domain/entities/transaction.dart';
 import 'package:smart_wallet/features/transaction/domain/repositories/transaction_repository.dart';
 import 'package:smart_wallet/features/transaction/domain/usecases/get_transactions.dart';
 
 class MockTransactionRepository extends Mock implements TransactionRepository {}
 
 void main() {
-  GetTransactions usecase;
-  MockTransactionRepository mockTransactionRepository;
+  late GetTransactions usecase;
+  late MockTransactionRepository mockTransactionRepository;
   setUp(() {
     mockTransactionRepository = MockTransactionRepository();
     usecase = GetTransactions(mockTransactionRepository);
+  });
+
+  final tTransactions = <Transaction>[];
+
+  test('should get transactions from the repository', () async {
+    when(
+      mockTransactionRepository.getTransactions(),
+    ).thenAnswer((_) async => Right([]));
+
+    final result = await usecase();
+
+    expect(result, Right(tTransactions));
+
+    verify(mockTransactionRepository.getTransactions());
+
+    verifyNoMoreInteractions(mockTransactionRepository);
   });
 }
