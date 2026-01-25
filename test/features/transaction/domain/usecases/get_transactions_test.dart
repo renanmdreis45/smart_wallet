444993d337e5705677062497790342e5eb042ab1
Renan Martins <renanmdreis@alu.ufc.br>
2026-01-25 19:44:36 -0300

    fix (test): fix expect between result and Right(tTransactions)

--- test/features/transaction/domain/usecases/get_transactions_test.dart
+++ test/features/transaction/domain/usecases/get_transactions_test.dart
@@ -1,13 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:smart_wallet/core/error/failure.dart';
 import 'package:smart_wallet/features/transaction/domain/entities/transaction.dart';
 import 'package:smart_wallet/features/transaction/domain/repositories/transaction_repository.dart';
 import 'package:smart_wallet/features/transaction/domain/usecases/get_transactions.dart';
-import 'package:mockito/annotations.dart';
 
 import 'get_transactions_test.mocks.dart';
-
 
 @GenerateMocks([TransactionRepository])
 void main() {
@@ -23,11 +23,11 @@ void main() {
   test('should get transactions from the repository', () async {
     when(
       mockTransactionRepository.getTransactions(),
-    ).thenAnswer((_) async => Right([]));
+    ).thenAnswer((_) async => Right(tTransactions));
 
     final result = await usecase.execute();
 
-    expect(result, Right(tTransactions));
+    expect(result, Right<Failure, List<Transaction>>(tTransactions));
 
     verify(mockTransactionRepository.getTransactions());
 
