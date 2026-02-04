ff6a16c40eff88c0383c470e32199cbae60c10cb
Renan Martins <renanmdreis@alu.ufc.br>
2026-02-03 22:53:23 -0300

    refactor: change type name

--- lib/core/usescases/usecase.dart
+++ lib/core/usescases/usecase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:smart_wallet/core/error/failure.dart';
 
-abstract class NoParamsUseCase <Type> {
-  Future<Either<Failure, Type>> call();
+abstract class NoParamsUseCase <T> {
+  Future<Either<Failure, T>> call();
 }
