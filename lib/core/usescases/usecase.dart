f80e1c34c3764d2ce9701bb4f3e25c9bf238b003
Renan Martins <renanmdreis@alu.ufc.br>
2026-02-04 23:03:57 -0300

    refactor: add params on usecase

--- lib/core/usescases/usecase.dart
+++ lib/core/usescases/usecase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:smart_wallet/core/error/failure.dart';
 
-abstract class NoParamsUseCase <T> {
-  Future<Either<Failure, T>> call();
+abstract class UseCase<T, Params> {
+  Future<Either<Failure, T>> call(Params params);
 }
--- lib/features/transaction/domain/usecases/get_transactions.dart
+++ lib/features/transaction/domain/usecases/get_transactions.dart
@@ -1,16 +1,23 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:smart_wallet/core/error/failure.dart';
 import 'package:smart_wallet/core/usescases/usecase.dart';
 import 'package:smart_wallet/features/transaction/domain/entities/transaction.dart';
 import 'package:smart_wallet/features/transaction/domain/repositories/transaction_repository.dart';
 
-class GetTransactions implements NoParamsUseCase<List<Transaction>> {
+class GetTransactions implements NoParamsUseCase<List<Transaction>, NoParams> {
   final TransactionRepository repository;
 
   GetTransactions(this.repository);
 
   @override
-  Future<Either<Failure, List<Transaction>>> call() async {
+  Future<Either<Failure, List<Transaction>>> call(NoParams params) async {
     return await repository.getTransactions();
   }
+}
+
+class NoParams extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
 }
