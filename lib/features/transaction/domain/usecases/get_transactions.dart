3bc2b9b545aea890812e8d0ea5103aa81909300e
Renan Martins <renanmdreis@alu.ufc.br>
2026-02-04 22:48:07 -0300

    refactor: implements NoParamsUseCase on GetTransactions usecase

--- lib/features/transaction/domain/usecases/get_transactions.dart
+++ lib/features/transaction/domain/usecases/get_transactions.dart
@@ -1,13 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:smart_wallet/core/error/failure.dart';
+import 'package:smart_wallet/core/usescases/usecase.dart';
 import 'package:smart_wallet/features/transaction/domain/entities/transaction.dart';
 import 'package:smart_wallet/features/transaction/domain/repositories/transaction_repository.dart';
 
-class GetTransactions {
+class GetTransactions implements NoParamsUseCase<List<Transaction>> {
   final TransactionRepository repository;
 
   GetTransactions(this.repository);
 
+  @override
   Future<Either<Failure, List<Transaction>>> call() async {
     return await repository.getTransactions();
   }
