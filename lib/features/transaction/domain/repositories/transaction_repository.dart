38a99ffca1606aad6006ef0063f9d7278b59b48b
Renan Martins <renanmdreis@alu.ufc.br>
2026-02-05 22:59:20 -0300

    refactor: fix transaction entity referencies

--- lib/features/transaction/domain/repositories/transaction_repository.dart
+++ lib/features/transaction/domain/repositories/transaction_repository.dart
@@ -3,6 +3,6 @@ import 'package:smart_wallet/core/error/failure.dart';
 import 'package:smart_wallet/features/transaction/domain/entities/transaction.dart';
 
 abstract class TransactionRepository {
-  Future<Either<Failure, List<Transaction>>> getTransactions();
-  Future<Either<Failure, Transaction>> getTransactionById(String id);
+  Future<Either<Failure, List<TransactionEntity>>> getTransactions();
+  Future<Either<Failure, TransactionEntity>> getTransactionById(String id);
 }
--- lib/features/transaction/domain/usecases/get_transactions.dart
+++ lib/features/transaction/domain/usecases/get_transactions.dart
@@ -5,13 +5,13 @@ import 'package:smart_wallet/core/usescases/usecase.dart';
 import 'package:smart_wallet/features/transaction/domain/entities/transaction.dart';
 import 'package:smart_wallet/features/transaction/domain/repositories/transaction_repository.dart';
 
-class GetTransactions implements UseCase<List<Transaction>, NoParams> {
+class GetTransactions implements UseCase<List<TransactionEntity>, NoParams> {
   final TransactionRepository repository;
 
   GetTransactions(this.repository);
 
   @override
-  Future<Either<Failure, List<Transaction>>> call(NoParams params) async {
+  Future<Either<Failure, List<TransactionEntity>>> call(NoParams params) async {
     return await repository.getTransactions();
   }
 }
