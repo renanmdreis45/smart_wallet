3f2775a42dde4d1666e2cdc801d291a97d8d9632
Renan Martins <renanmdreis@alu.ufc.br>
2026-02-06 23:52:51 -0300

    feat: add fromJson transaction test

--- test/features/transaction/data/models/transaction_model_test.dart
+++ test/features/transaction/data/models/transaction_model_test.dart
@@ -1,9 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:smart_wallet/features/transaction/data/models/transaction_model.dart';
 import 'package:smart_wallet/features/transaction/domain/entities/transaction.dart';
 
 void main() {
-  final tTransactionModel = TransactionModel();
+  final tTransactionModel = TransactionModel(
+    id: '1',
+    amount: 100.0,
+    date: DateTime.now(),
+    description: 'Test Transaction',
+  );
   test('should be a subclass of TransactionEntity', () async {
     expect(tTransactionModel, isA<TransactionEntity>());
+  });
+
+  group('fromJson', () {
+    test('should return a valid model when the JSON is provided', () async {
+      final Map<String, dynamic> json = {
+        'id': '1',
+        'amount': 100.0,
+        'date': DateTime.now().toIso8601String(),
+        'description': 'Test Transaction',
+      };
+      final model = TransactionModel.fromJson(json);
+      expect(model, tTransactionModel);
+    });
   });
 }
